bb27e5b8a3c2e52a0a00b5a45f23799d9d0fa363
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-03-06 16:28:37 +0530

    Importing Exporting modules
    We have used import export to exchange modules between files, from the one exporting it, to the one importing it.

---
+++ importExport/export.js
@@ -0,0 +1,12 @@
+// This is the export file.
+const add=(a,b)=>{
+	 a+b;
+}
+
+const subs=(a,b)=>{
+	a-b;
+}
+const mult=(a,b)=>{
+	a*b;
+}
+export {add,subs,mult};
---
+++ importExport/import.js
@@ -0,0 +1,6 @@
+//This is the import file
+//import {add,subs,mult} from "./import.js";
+import * as object from "./import.js";
+console.log(object.add(2,3));
+console.log(object.subs(2,3));
+console.log(object.mult(2,3));
